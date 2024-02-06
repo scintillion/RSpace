4d46186b9e29f5c8f086d5b2f8a295145f799a01
scintillion <43735913+scintillion@users.noreply.github.com>
2024-02-06 08:58:22 -0500

    RS

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -39,8 +39,6 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const FPS_BROWSER_APP_PROFILE_STRING: string;
-	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const FP_NO_HOST_CHECK: string;
 	export const FSHARPINSTALLDIR: string;
 	export const HOME: string;
@@ -155,8 +153,6 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		FPS_BROWSER_APP_PROFILE_STRING: string;
-		FPS_BROWSER_USER_PROFILE_STRING: string;
 		FP_NO_HOST_CHECK: string;
 		FSHARPINSTALLDIR: string;
 		HOME: string;
