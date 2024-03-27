81b9e9971a0a26d8cc12783173cf47627375fceb
scintillion <43735913+scintillion@users.noreply.github.com>
2024-03-26 22:30:16 -0400

    Add files via upload

---
+++ src/components/tiles/RTile.ts
@@ -0,0 +1,11 @@
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+@customElement('r-tile')
+export class RTile extends LitElement {
+  color = "cyan";
+
+  render(){
+    return html`<h2 style=color:{this.color} >Hello from RTile!</h2>`;
+  }
+}
