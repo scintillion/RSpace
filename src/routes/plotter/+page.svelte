0bb1aea9df2641ba0b74eef9ff81ca475320e8ad
Mahir Molai <mahir@molai.dev>
2025-12-19 17:17:35 +0500

    further cleanup

--- src/routes/plotter/+page.svelte
+++ src/routes/plotter/+page.svelte
@@ -9,25 +9,25 @@
 		' T\ta|name:Top|\ts|background:magenta|height:10vh|width:100vw|\t',
 		' T\ta|name:Bottom|\ts|display:flex|row:1|background:none|align-items:center|justify-content:space-evenly|\t',
 		'  T\ta|name:Left|inner:I am the left side|\ts|background:orange|width:20vw|height:90vh|display:flex|column:1|gap:5|align-items:center|justify-content:center|\t',
-		'   RndBtn\ta|name:Button|inner:Click|redirect:https://moocode.lol/|\ts|width:110|height:50|background:#1e1e1e|color:white|\t',
+		'   RndBtn\ta|name:Button|inner:Click|redirect:https://google.com/|\ts|width:110|height:50|background:#1e1e1e|color:white|\t',
 		'  T\ta|name:Middle|inner:I am the middle|\ts|background:cyan|display:flex|width:60vw|height:90vh|\t',
 		'  T\ta|name:Right|inner:I am the right side|\ts|background:yellow|width:20vw|height:90vh|\t'
 	];
 
-	const List: RS1.TileList = new RS1.TileList(TileStrings); // remove temporarily
+	const List: RS1.TileList = new RS1.TileList(TileStrings);
 
 	onMount(() => {
-		console.log ('onMount Plotter!');
-		console.log ('TileList Str =\n' + List.toString ());
 		let tiles = document.querySelector('.tiles');
 		if (tiles) {
 			const plotter: Plotter = new Plotter(List, tiles as HTMLDivElement);
 			plotter.PlotTiles();
-		} // RndBtn	a|name:Button|inner:Click|redirect:https://moocode.lol/|	s|width:110|height:50|background:#1e1e1e|color:white|
+		}
 	});
 </script>
 
-<div class="tiles" />
+<div class="tiles">
+	<!-- rendered html from plotter goes here -->
+</div>
 
 <style>
 	.tiles {
